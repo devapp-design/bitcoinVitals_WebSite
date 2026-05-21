33fc237bc601ea4b74a2b55e7347948c59d2f533
devapp-design <dev@bitcoinvitals.com>
2026-05-21 17:29:45 +0500

    public contents for dynamic link

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,9 @@ import About from "./pages/About";
 import Contact from "./pages/Contact";
 import Privacy from "./pages/Privacy";
 import Terms from "./pages/Terms";
+import AddressPage from "./pages/AddressPage";
+import TxPage from "./pages/TxPage";
+import BlockPage from "./pages/BlockPage";
 
 function App() {
   return (
@@ -20,6 +23,11 @@ function App() {
             <Route path="/contact" element={<Contact />} />
             <Route path="/privacy" element={<Privacy />} />
             <Route path="/terms" element={<Terms />} />
+            <Route path="/address/:address" element={<AddressPage />} />
+
+            <Route path="/tx/:txid" element={<TxPage />} />
+
+            <Route path="/block/:hash" element={<BlockPage />} />
           </Routes>
         </main>
         <Footer />
